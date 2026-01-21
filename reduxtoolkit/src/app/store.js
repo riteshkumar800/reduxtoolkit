8c5c61fe6fb070adcc1a926cf8fdf1007d9c6ea9
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-01-21 20:41:35 +0530

    final code

---
+++ reduxtoolkit/src/app/store.js
@@ -0,0 +1,10 @@
+// import {useSelector, useDispatch} from 'react-redux'
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice'
+
+export const store=configureStore({
+    reducer:todoReducer
+})
+
+
+
--- reduxtoolkit/src/components/Todos.jsx
+++ reduxtoolkit/src/components/Todos.jsx
@@ -3,6 +3,17 @@ import {useSelector, useDispatch} from 'react-redux'
 import {removeTodo} from '../features/todo/todoSlice'
 
 
+
+
+const Todos=()=>{
+
+    const todos=useSelector(state=>state.todos)
+    const dispatch=useDispatch()
+
+
+
+
+
 return (
     <>
     <div>Todos</div>
@@ -37,6 +48,7 @@ return (
       </ul>
     </>
   )
+}
 
 
 export default Todos
